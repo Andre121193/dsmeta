a2f950b5526ed32863296c32cf4c3ccc8511a868
andre <andre1993andre@hotmail.com>
2022-09-07 11:09:09 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return (
-    <h1>Ola mundo</h1>
+    <>
+      <h1>Ola mundo</h1>
+      <NotificationButton />
+    </>
   )
 }
 

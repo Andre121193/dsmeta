22a18d6b5321d337fd57b2a7a2efc381288ae035
andre <andre1993andre@hotmail.com>
2022-09-08 23:14:50 -0300

    alterando formato mensagem de texto

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -38,7 +38,7 @@ public class SmsService {
 		String date = sale.getDate().getMonthValue() + "/" +  sale.getDate().getYear();
 		
 		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
-			    + " com um total de R$ " + String.format("%.0f", sale.getAmount());
+			    + " com um total de R$ " + new DecimalFormat("#,##0.00").format(sale.getAmount());
 
 		Twilio.init(twilioSid, twilioKey);
 
